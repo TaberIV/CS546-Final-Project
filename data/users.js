dabcf3437dfd3535590dc33734758b3ea8198a73
Taber McFarlin <tabermcfarlin@gmail.com>
2018-05-04 17:52:44 -0400

    Idk

--- data/users.js
+++ data/users.js
@@ -1,6 +1,5 @@
 const bcrypt = require('bcrypt');
 
-<<<<<<< HEAD
 // Replace with mongo things
 const users = [
 	{
@@ -31,43 +30,11 @@ const users = [
 		sessionIDs: []
 	}	
 ];
-=======
-
-// const users = [
-// 	{
-// 		username: "masterdetective123",
-// 		firstname: "Sherlock",
-// 		lastname: "Holmes",
-// 		profession: "Detective",
-// 		bio: 'Sherlock Holmes (/ˈʃɜːrlɒk ˈhoʊmz/) is a fictional private detective created by British author Sir Arthur Conan Doyle. Known as a "consulting detective" in the stories, Holmes is known for a proficiency with observation, forensic science, and logical reasoning that borders on the fantastic, which he employs when investigating cases for a wide variety of clients, including Scotland Yard.',
-// 		hashedpassword: "$2a$16$7JKSiEmoP3GNDSalogqgPu0sUbwder7CAN/5wnvCWe6xCKAKwlTD.",
-// 		sessionIDs: []
-// 	},
-// 	{
-// 		username: "lemon",
-// 		firstname: "Elizabeth",
-// 		lastname: "Lemon",
-// 		profession: "Writer",
-// 		bio: 'Elizabeth Miervaldis "Liz" Lemon is the main character of the American television series 30 Rock. She created and writes for the fictional comedy-sketch show The Girlie Show or TGS with Tracy Jordan.',
-// 		hashedpassword: "$2a$16$SsR2TGPD24nfBpyRlBzINeGU61AH0Yo/CbgfOlU1ajpjnPuiQaiDm",
-// 		sessionIDs: []
-// 	},
-// 	{
-// 		username: "theboywholived",
-// 		firstname: "Harry",
-// 		lastname: "Potter",
-// 		profession: "Student",
-// 		bio: "Harry Potter is a series of fantasy novels written by British author J. K. Rowling. The novels chronicle the life of a young wizard, Harry Potter, and his friends Hermione Granger and Ron Weasley, all of whom are students at Hogwarts School of Witchcraft and Wizardry . The main story arc concerns Harry's struggle against Lord Voldemort, a dark wizard who intends to become immortal, overthrow the wizard governing body known as the Ministry of Magic, and subjugate all wizards and Muggles.",
-// 		hashedpassword: "$2a$16$4o0WWtrq.ZefEmEbijNCGukCezqWTqz1VWlPm/xnaLM8d3WlS5pnK",
-// 		sessionIDs: []
-// 	}	
-// ];
 
 async function getAllUsers() {
 	// Insert mongo things
 	// return users;
 }
->>>>>>> 084bc7c0a48684054985626fc262f03d613420fe
 
 async function getUserByUsername(username) {
 	if (!username ||typeof username != 'string')
