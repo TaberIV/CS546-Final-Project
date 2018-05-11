50cc79e8b9c9a3d0db3bffdd2cbdb5ec7827597f
Taber McFarlin <tabermcfarlin@gmail.com>
2018-05-10 20:00:42 -0400

    Movie ratings completed

--- code/data/reviews.js
+++ code/data/reviews.js
@@ -4,14 +4,49 @@ const reviews = mongoCollections.reviews;
 const uuid = require('uuid/v4');
 
 async function addReview(reviewInfo) {
-    // Ensure all required info is present
     if (!reviewInfo.rating)
         throw "Must provite a rating";
 
-    console.log(reviewInfo);
+    let newReview = {
+        _id: uuid(),
+        movie: reviewInfo.movie,
+        user: reviewInfo.user,
+        rating: Number(reviewInfo.rating),
+        text: reviewInfo.reviewText
+    };
     
+    let reviewCollection = await reviews();
+    await reviewCollection.insertOne(newReview);
+
+    return newReview._id;
+}
+
+async function getReviewsByMovie(movieID) {
+    let reviewCollection = await reviews();
+    return (await reviewCollection.find({ movie: movieID })).toArray();
+}
+
+async function getAverageRating(movieID) {
+    let reviewCollection = await reviews();
+    let scores = await reviewCollection.find({ movie: movieID }, {_id: 0, rating: 1}).toArray();
+    
+    let average = 0;
+    let i = 0
+    for (i = 0; i < scores.length; i++)
+        average += scores[i].rating;
+    average /= i;
+
+    return average;
+}
+
+async function getReviewsByUser(userID) {
+    let reviewCollection = await reviews();
+    return (await reviewCollection.find({ user: userID })).toArray();
 }
 
 module.exports = {
-    addReview
+    addReview,
+    getReviewsByMovie,
+    getAverageRating,
+    getReviewsByUser
 }
